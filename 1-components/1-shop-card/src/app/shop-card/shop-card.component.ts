31a951a5fcc3732f1d2ece456a97737638485785
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-05-29 19:19:05 +0300

    feat: complete task 1-shop-card

--- 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
+++ 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ICartProduct } from '../../../../../shared/mocks/1-components/cart-product';
 
 @Component({
   selector: 'app-shop-card',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 
 export class ShopCardComponent {
 
+  @Input() product: ICartProduct = {} as ICartProduct;
+
+  @Output() increment: EventEmitter<void> = new EventEmitter();
+  @Output() decrement: EventEmitter<void> = new EventEmitter();
+
+  public incrementProductInCart(): void {
+    this.increment.emit();
+  }
+
+  public decrementProductInCart(): void {
+    this.decrement.emit();
+  }
 }
