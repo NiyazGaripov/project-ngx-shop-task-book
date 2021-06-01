72d2a2fe58da5ebe86298f92e726811af7efe63d
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-06-01 21:28:09 +0300

    feat: implements RatePipe

--- 2-pipe/1-transforms-with-pipe/src/app/pipes/rate.pipe.ts
+++ 2-pipe/1-transforms-with-pipe/src/app/pipes/rate.pipe.ts
@@ -6,6 +6,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 // @ts-ignore
 export class RatePipe implements PipeTransform {
   public transform(value: number): number | undefined {
-    return 0;
+    const fractionalPart = value % 1;
+
+    if (fractionalPart < 0.25) {
+      return Math.floor(value);
+    } else if (fractionalPart >= 0.25 && fractionalPart < 0.75) {
+      return Math.floor(value) + 0.5;
+    } else if (fractionalPart > 0.75) {
+      return Math.ceil(value);
+    }
   }
 }
