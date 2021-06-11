e47427fe22e04b74fb5eb1596544b203f460d342
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-06-11 21:30:48 +0300

    feat: adds categories, categoryChange event, currentName and creates methods hover, mouseLeave, redirectTo

--- 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
+++ 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
@@ -1,7 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ICategory } from '../../../../../shared/mocks/3-directives/categories';
 
 @Component({
   selector: 'ngx-shop-side-menu',
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+
+  @Input() public categories: ICategory[] = [];
+
+  @Output() public categoryChange: EventEmitter<string> = new EventEmitter<string>();
+
+  public currentName: string | null = null;
+
+  public hover(name: string): void {
+    this.currentName = name;
+  }
+
+  public mouseLeave(): void {
+    this.currentName = null;
+  }
+
+  public redirectTo(id: string): void {
+    this.categoryChange.emit(id);
+  }
+}
