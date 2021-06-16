a404bb5754887ea169bcdde9f3082d732d523c19
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-06-16 20:29:54 +0300

    feat: creates method getProducts

--- 4-services/1-home-page/src/app/services/products/products.service.ts
+++ 4-services/1-home-page/src/app/services/products/products.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from '../../../../../../shared/mocks/4-services/products';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  public getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(`api/products/suggestion`);
+  }
+}
