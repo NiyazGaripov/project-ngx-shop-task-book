1a177e24480e2298ebeb249a8b3d63e289ae42a5
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-06-17 23:01:12 +0300

    feat: adds inputs values and creates methods

--- 4-services/1-home-page/src/app/home-page/home-page.component.ts
+++ 4-services/1-home-page/src/app/home-page/home-page.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 @Component({
   selector: 'ngx-shop-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.sass'],
 })
 // @ts-ignore
-export class HomePageComponent {}
+export class HomePageComponent {
+  constructor() {}
+
+  @Input() public products = null;
+  @Input() public categories = null;
+
+  public goToBasket(): void {
+  }
+
+  public goToProduct(): void {
+  }
+
+  public redirectTo(evt: string): void {
+  }
+}
