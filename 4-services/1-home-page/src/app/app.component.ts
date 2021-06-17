f30f5bd304c9b7ee1c42cb278a1106c709574e79
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-06-17 22:59:31 +0300

    feat: adds services, gets products and categories

--- 4-services/1-home-page/src/app/app.component.ts
+++ 4-services/1-home-page/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../../../../shared/mocks/4-services/products';
+import { ICategory } from '../../../../shared/mocks/4-services/categories';
+import { Observable } from 'rxjs';
+import { ProductsService } from './services/products/products.service';
+import { CategoriesService } from './services/categories/category.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 // @ts-ignore
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'Компонент домашней страницы';
+
+  public products$: Observable<IProduct[]> = null;
+  public categories$: Observable<ICategory[]> = null;
+
+  constructor(
+    private productsService: ProductsService,
+    private categoriesService: CategoriesService,
+  ) {}
+
+  ngOnInit() {
+    this.products$ = this.productsService.getProducts();
+    this.categories$ = this.categoriesService.getCategories();
+  }
 }
